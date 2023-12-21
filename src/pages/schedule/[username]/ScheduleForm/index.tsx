1a582760e1d4708e6ce7ae2277a1b1a61d1d6b42
Kevin <kevinsilvarj10@gmail.com>
2023-12-21 17:42:08 -0300

    feat: Fazer Estrutura visual e algumas funcionalidades da página Schedule

---
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -0,0 +1,8 @@
+// import { CalendarStep } from './CalendarStep'
+import { ConfirmStep } from './ConfirmStep'
+
+/* <CalendarStep /> */
+
+export function ScheduleForm() {
+  return <ConfirmStep />
+}
